ebc70d201f7c7e11ec82bb896fede7709193828f
Brian Elizondo <brian_elizondo@msn.com>
2022-04-29 20:10:52 -0400

    Part One => Create a skeleton Flask App, import User model an create the tables using SQLAlchemy. Install FlaskDebugToolbar

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 """Blogly application."""
 
-from flask import Flask
+from crypt import methods
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
 
 app = Flask(__name__)
@@ -8,6 +10,8 @@ app.config['SECRET_KEY'] = "abcd1234"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
