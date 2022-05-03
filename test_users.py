e77920b7add52549069376e6df5ce9d28de29471
Brian Elizondo <brian_elizondo@msn.com>
2022-05-03 17:57:32 -0400

    Part Two => Testing to update any broken tests and add more testing

--- test_users.py
+++ test_users.py
@@ -2,7 +2,7 @@ from app import app
 from unittest import TestCase
 
 class UsersTestCase(TestCase):
-    # Integration Tests
+    # Integration Tests for Users
     def test_users_redirect(self):
         with app.test_client() as client:
             res = client.get('/')
@@ -51,8 +51,49 @@ class UsersTestCase(TestCase):
     
     def test_user_delete_process(self):
         with app.test_client() as client:
-            res = client.get('/users/1/delete')
+            res = client.get('/users/3/delete')
             html = res.get_data(as_text=True)
 
             self.assertEqual(res.status_code, 302)
             self.assertEqual(res.location, '/users')
+
+    
+    # Integration Tests for Posts
+    def test_post_form(self):
+        with app.test_client() as client:
+            res = client.get('/users/1/posts/new')
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<label for="post_title" class="form-label">Title</label>', html)
+
+    def test_post_process(self):
+        with app.test_client() as client:
+            res = client.post('/users/1/posts/new', data={'post_title': 'test title', 'post_content': 'test content'})
+
+            self.assertEqual(res.status_code, 302)
+            self.assertEqual(res.location, '/users/1')
+
+    def test_post_details(self):
+        with app.test_client() as client:
+            res = client.get('/posts/1')
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<title>SQLAlchemy - Blogly - Post Details</title>', html)
+
+    def test_post_edit_form(self):
+        with app.test_client() as client:
+            res = client.get('/posts/1/edit')
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<h1>Edit Post</h1>', html)
+
+    def test_post_delete_process(self):
+        with app.test_client() as client:
+            res = client.get('/posts/1/delete')
+
+            self.assertEqual(res.status_code, 302)
+            self.assertEqual(res.location, '/users/1')
+
